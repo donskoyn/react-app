373e07a3b304b3ebc0bb2e3e81b21574a29841db
donskoyn <donskoynik1995@gmail.com>
2020-08-04 14:21:01 +0300

    Updates

---
+++ precache-manifest.6358eb8caa4a4265eb92bf4343d58ba6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c9d3bea5e13c63387ac35215066a36",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "651ca7f107a3aa23e900",
+    "url": "/react-app/static/css/main.866be9ac.chunk.css"
+  },
+  {
+    "revision": "f92d9cefddd339df80cb",
+    "url": "/react-app/static/js/2.c378e63f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.c378e63f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "651ca7f107a3aa23e900",
+    "url": "/react-app/static/js/main.577a865e.chunk.js"
+  },
+  {
+    "revision": "605cf62042abf25f1d08",
+    "url": "/react-app/static/js/runtime-main.056c36fa.js"
+  }
+]);
